dd7998f5ccac9f594c2d93173ad1870e98116d88
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-11 14:59:31 +0000

    change layout of cubes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import './App.css';
 import * as THREE from 'three';
 
 class App extends Component {
+
+  state = {
+  }
 
   componentDidMount() {
     let scene = new THREE.Scene();
@@ -16,43 +19,69 @@ class App extends Component {
 
     window.addEventListener('resize', () => {
       renderer.setSize(window.innerWidth, window.innerHeight);
-      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.aspect = this.mount.clientWidth / this.mount.clientHeight;
 
       camera.updateProjectionMatrix();
     })
 
-    let geometry = new THREE.BoxGeometry();
-    let material = new THREE.MeshLambertMaterial({ color: 0x00ff00 })
-    let cube = new THREE.Mesh(geometry, material);
-    let light = new THREE.PointLight(0xFFFFFF, 1, 500)
-    light.position.set(10, 0, 25);
-    scene.add(light);
+    const makeCubeInstance = (geometry, colour, x, y) => {
+      const material = new THREE.MeshPhongMaterial({ color: colour });
 
-    scene.add(cube);
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      cube.position.x = x;
+      cube.position.y = y
+
+      return cube;
+    }
+
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    const cubes = [
+      makeCubeInstance(geometry, 0xFFDFD3, 0, 2),
+      makeCubeInstance(geometry, 0x957DAD, 0, 0),
+      makeCubeInstance(geometry, 0xFEC8D8, 0, -2)
+    ]
+
+    let light = new THREE.PointLight(0xFFFFFF, 1, 250)
+    light.position.set(0, 0, 25);
+    scene.add(light);
 
     camera.position.z = 5;
 
     function animate() {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      cubes.forEach(cube => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+      })
+
       renderer.render(scene, camera);
     }
+
+    console.log(this.camera)
 
     animate();
   }
 
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
         </header>
-        <body>
-          <div ref={ref => (this.mount = ref)} />
-        </body>
+        <div ref={ref => (this.mount = ref)} />
       </div>
     );
   }
+
+
 
 }
 
