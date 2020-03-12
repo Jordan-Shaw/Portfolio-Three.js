2cb83246e96158ee80ca853a02734e7a25e3fb58
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-03-12 22:28:54 +0000

    Add a header component

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Jordan Shaw</h1>
+      <h3>Fullstack Developer</h3>
+    </div>
+  )
+}
